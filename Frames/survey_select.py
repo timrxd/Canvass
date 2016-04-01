cb8e7f481bc003dd31c2363ca92522aaea2d44e6
timrxd <timxdowd@gmail.com>
2016-04-01 13:50:34 -0400

    bit broken

--- Frames/survey_select.py
+++ Frames/survey_select.py
@@ -7,6 +7,9 @@ except ImportError:
     # for Python3
     from tkinter import *
 
+from .scroll_frame import ScrollFrame
+
+
 class SelectSurvey(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
@@ -25,36 +28,44 @@ class SelectSurvey(Frame):
         # Why are the weights different? FUCK
 
         # Row 3 Headers
-        self.top_bar.grid_columnconfigure(0, weight=6)
+        self.top_bar.grid_columnconfigure(0, weight=1)
         self.top_bar.grid_columnconfigure(1, weight=1)
         self.top_bar.grid_columnconfigure(2, weight=1)
-        self.top_bar.grid_columnconfigure(3, weight=4)
+        self.top_bar.grid_columnconfigure(3, weight=1)
         self.top_bar.grid_columnconfigure(4, weight=1)
 
         h_row = 3
-        self.doc_name_label = Label(self.top_bar, text="Survey Name", bg="grey", relief=RAISED, anchor="w")
+        self.doc_name_label = Label(self.top_bar, text="Survey Name", bg="lightgray", relief=RAISED, anchor="w")
         self.doc_name_label.grid(row=h_row, column=0, sticky="nswe")
 
-        self.last_mod = Label(self.top_bar, text="Last Modified", bg="grey", relief=RAISED, anchor="w")
+        self.last_mod = Label(self.top_bar, text="Last Modified", bg="lightgray", relief=RAISED, anchor="w")
         self.last_mod.grid(row=h_row, column=1, sticky="nswe")
 
-        self.responses = Label(self.top_bar, text="Responses", bg="grey", relief=RAISED, anchor="w")
+        self.responses = Label(self.top_bar, text="Responses", bg="lightgray", relief=RAISED, anchor="w")
         self.responses.grid(row=h_row, column=2, sticky="nswe")
 
-        self.location = Label(self.top_bar, text="Location", bg="grey", relief=RAISED, anchor="w")
+        self.location = Label(self.top_bar, text="Location", bg="lightgray", relief=RAISED, anchor="w")
         self.location.grid(row=h_row, column=3, sticky="nswe")
 
-        self.date = Label(self.top_bar, text="Event Date", bg="grey", relief=RAISED, anchor="w")
+        self.date = Label(self.top_bar, text="Event Date", bg="lightgray", relief=RAISED, anchor="w")
         self.date.grid(row=h_row, column=4, sticky="nswe")
 
         # Survey Table
-        self.survey_table = Frame(self, bg="white")
+        self.scroll_table = ScrollFrame(self, bg="white")
+        self.scroll_table.pack(fill=BOTH, expand=TRUE)
+        self.survey_table = Frame(self.scroll_table, bg="white")
         self.survey_table.pack(fill=BOTH, expand=TRUE)
 
-        self.survey_table.grid_columnconfigure(0, weight=6)
+        # Scrollable WIP
+        self.scroll_table.vsb = Scrollbar(self.scroll_table, orient="vertical", command=self.survey_table.yview)
+        self.survey_table.configure(yscrollcommand=self.vsb.set)
+        self.scroll_table.vsb.pack(side="right", fill="y")
+        self.survey_table.pack(side="left", fill="both", expand=True)
+
+        self.survey_table.grid_columnconfigure(0, weight=1)
         self.survey_table.grid_columnconfigure(1, weight=1)
         self.survey_table.grid_columnconfigure(2, weight=1)
-        self.survey_table.grid_columnconfigure(3, weight=4)
+        self.survey_table.grid_columnconfigure(3, weight=1)
         self.survey_table.grid_columnconfigure(4, weight=1)
 
         # Eventually SQL call
@@ -65,6 +76,9 @@ class SelectSurvey(Frame):
                      ["Del Val", "2/21", "100", "Baltimore", "1/1"],
                      ["JP Case", "1/25", "88", "Flemington", "2/2"]
                     ]
+
+        test_data += test_data
+        test_data += test_data
 
         r = 0
         c = 0
