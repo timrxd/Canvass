2bede54faf8692f87c5537c643c33d1314af0c95
Tim Dowd <timxdowd@gmail.com>
2016-04-10 21:19:25 -0400

    Server is UP

--- sql_driver.py
+++ sql_driver.py
@@ -1,4 +1,18 @@
 import MySQLdb
 
-db = MySQLdb.connect("localhost", "monitor", "password", "temps")
-curs=db.cursor()
+# Open database connection
+db = MySQLdb.connect(host="144.126.12.11", port=3306, user="hpr", passwd="hunterdon", db="surveys")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Test
+cursor.execute("SELECT * FROM test;")
+data = cursor.fetchall()
+for line in data:
+    for val in line:
+        print(val)
+    print("")
+
+# disconnect from server
+db.close()
